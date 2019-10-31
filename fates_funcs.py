7ebd9ce024ad3492561f88e116baec5b31e46254
ckoven <ckoven@gmail.com>
2019-10-31 10:17:21 -0700

    re-regverted to pyngl version of popsicle plots

--- fates_funcs.py
+++ fates_funcs.py
@@ -1,8 +1,8 @@
 import numpy as np
+import Ngl
 import sys
-import matplotlib.pyplot as plt
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Rectangle,ConnectionPatch
+from map_funcs import get_workstation_type,parse_colormap,clear_oldest_x11_window,x11_window_list,pngdens,page_width,page_height,jupyter_avail
+
 
 def carea_allom(rest_fin, paramfin):
     ncohorts_max = len(rest_fin.variables['fates_CohortsPerPatch'][:])
@@ -28,8 +28,90 @@ def carea_allom(rest_fin, paramfin):
     return carea
 
 
-def popsicle_diagram(restartfile, param_file, pftcolors=['green','darkgreen'], stemcolor="brown", file=None, title=None, xtitle=None, ytitle=None, yrange=[0.,45.], colormap=None, title_charsize=0.75, aspect_ratio=None):
+def popsicle_diagram(restartfile, param_file, pftcolors, stemcolor=159, file=None, title=None, xtitle=None, ytitle=None, yrange=None, colormap=None, title_charsize=0.75, aspect_ratio=None, makepng=False, png_dens=pngdens, use_wks=None, showjupyter=False):
 
+    if use_wks == None:
+        plot_type = get_workstation_type(file)
+        
+        wks_res = Ngl.Resources()
+        if plot_type == 'x11':
+            wks_res.wkPause = False
+        elif plot_type == 'png':
+            wks_res.wkWidth = page_width * 100
+            wks_res.wkHeight = page_height * 100
+        else:
+            wks_res.wkPaperWidthF = page_width
+            wks_res.wkPaperHeightF = page_height
+            wks_res.wkOrientation = "portrait"
+            
+        if not colormap == None:
+            colormap = parse_colormap(colormap)
+            wks_res.wkColorMap = colormap
+            
+        wks = Ngl.open_wks(plot_type,file,wks_res)
+        if wks < 0 and plot_type == "x11":
+            clear_oldest_x11_window()
+            wks = Ngl.open_wks(plot_type,file,wks_res)
+    else:
+        wks = use_wks
+
+    resources = Ngl.Resources()
+
+    #cdkif plot_type != 'png':
+    resources.nglDraw = False
+
+    #cdkif plot_type != 'png':
+    resources.nglFrame = False
+
+
+    if title != None:
+        resources.tiMainString = title
+
+    if xtitle != None:
+        resources.tiXAxisString = xtitle
+
+    if ytitle != None:
+        resources.tiYAxisString = ytitle
+
+    if yrange != None:
+        resources.trYMinF = np.min(yrange)
+        resources.trYMaxF = np.max(yrange)
+
+    resources.trXMinF = 0.
+    resources.trXMaxF = 1.
+
+    resources.tiMainFontHeightF = 0.025 * title_charsize
+
+    resources.tmXBMajorLengthF        = 0.01
+    resources.tmXBMajorOutwardLengthF = 0.01
+    resources.tmYLMajorLengthF        = 0.01    
+    resources.tmYLMajorOutwardLengthF = 0.01
+
+    resources.tmXBMinorOn    = False
+    resources.tmYLMinorOn    = False
+
+    resources.tmXTOn          = False
+    resources.tmYROn          = False
+    resources.tmXBOn          = False
+    resources.tmXBLabelsOn    = False
+
+    if aspect_ratio != None:
+        if aspect_ratio > 1.:
+            resources.vpHeightF =  0.6  / aspect_ratio
+        else:
+            resources.vpWidthF =  0.6  * aspect_ratio
+
+    resources.trXMinF = 0.
+    resources.trXMaxF = 1.
+    if yrange == None:
+        yrange = [0, np.ma.max(restartfile.variables['fates_height'][:])]
+    resources.trYMinF = np.min(yrange)
+    resources.trYMaxF = np.max(yrange)
+
+    dummies = np.ma.masked_all(3)
+    plot = Ngl.xy(wks, dummies, dummies, resources)
+
+    #### the main logic all goes here.  ####
 
     # first calculate the crown areas of each cohort
     cohort_crownareas = carea_allom(restartfile, param_file)
@@ -37,18 +119,23 @@ def popsicle_diagram(restartfile, param_file, pftcolors=['green','darkgreen'], s
     max_coh_per_patch = 100
     max_cohorts = len(restartfile.variables['fates_CohortsPerPatch'][:])
     max_patches = max_cohorts / max_coh_per_patch
-    rects = []
-    lines = []
-    coordsA = "data"
-    maxcanlev = restartfile.variables['fates_canopy_layer'][:].max()
-    cohort_rhs = np.zeros(maxcanlev)
-    ## iterate over patches
-    patch_area_sofar = 0.
-    #
-    for i in range(max_patches):
-        ## draw thin vertical line at patch lower boundary edge to delineate patches
-        patch_lower_edge = 1.-patch_area_sofar - restartfile.variables['fates_area'][i*max_coh_per_patch]/1e4
-        patchseparator = ConnectionPatch((patch_lower_edge,0),(patch_lower_edge,200),coordsA)
+    try:
+        maxcanlev = restartfile.variables['fates_canopy_layer'][:].max()
+        cohort_rhs = np.zeros(maxcanlev)
+        ## iterate over patches
+        patch_area_sofar = 0.
+        vline_res = Ngl.Resources
+        vline_res.gsLineThicknessF = .01
+        #
+        crown_res = Ngl.Resources()
+        crown_res.gsLineColor            = "black"
+        crown_res.gsLineThicknessF       = .01
+        crown_res.gsEdgesOn              = True
+        crown_res.gsFillOpacityF         = 0.5
+        #
+        shadow_res = Ngl.Resources()
+        shadow_res.gsFillColor            = "black"
+        shadow_res.gsEdgesOn              = False
         #
         stem_res1 = Ngl.Resources()
         stem_res1.gsLineThicknessF       = 1.
@@ -104,11 +191,24 @@ def popsicle_diagram(restartfile, param_file, pftcolors=['green','darkgreen'], s
             ## draw cohort as popsicle
             #
             patch_area_sofar = patch_area_sofar + restartfile.variables['fates_area'][i*max_coh_per_patch]/1e4
-        # 
-    fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
-    for box in rects:
-        ax.add_artist(box)
-    ax.set_xlim(0, 1.)
-    ax.set_ylim(yrange[0], yrange[1])
-    #
-    plt.show()
+
+    except:
+        print('that didnt work')
+    if use_wks == None:
+        Ngl.draw(plot)
+        Ngl.frame(wks)
+    
+        if not file==None:
+            Ngl.delete_wks(wks)
+            #
+            if makepng or showjupyter:
+                pdf_to_png(file, density=png_dens)
+            if jupyter_avail and showjupyter:
+                print(' ')
+                print('showing file '+file)
+                display(Image(file+'.png'))
+        else:
+            x11_window_list.append(wks)
+    else:
+        return plot
+
